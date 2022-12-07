9d03e1daa9e1dd41d8ba96b546a4d92dc1a14885
Valentin <valentinarpin11@live.fr>
2022-12-07 14:33:05 +0100

    feat:[Add] tailwindcss

---
+++ src/_components/Navbar.tsx
@@ -0,0 +1,12 @@
+import React, { FunctionComponent} from 'react';
+
+const Navbar:FunctionComponent =() => {
+
+    return (
+        <div>
+    test
+        </div>
+    )
+}
+
+export default Navbar
